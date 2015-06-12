e7536469d95cc2951c25386731114bc54ef3b7c5
Lei Yang <yltt1234512@gmail.com>
2015-06-12 13:06:56 +0800

    Test script for GitHub Webhook

---
+++ server.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import json
+
+listenAddr = "128.199.244.33"
+listenPort = 7654
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        data = json.loads(post_body)
+        print(data)
+
+
+myServer = HTTPServer((listenAddr, listenPort), MyServer)
+print("Syncing tool prototype")
+print(time.asctime(), "Server Starts - %s:%s" % (listenAddr, listenPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Picture Server Stops - %s:%s" % (listenAddr, listenPort))
