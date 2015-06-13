97e9281d51d0a44d718ea987ca7466f818ae6cc7
Lei Yang <yltt1234512@gmail.com>
2015-06-14 01:31:25 +0800

    add metadata in the title

--- server.py
+++ server.py
@@ -6,7 +6,8 @@ import requests
 listenAddr = "128.199.82.190"
 listenPort = 7654
 secretKey = ""  # TODO:implement the secure feature of GitHub web hook (verify SHA1 signature)
-pagureToken = "token 21HEK7SCCDNT12APRN00YJIY4AIYZM3DLXPNT8NP1275FA9MRWJZDM2ICWT8MYJX"  # TODO: this is a test propose token
+pagureToken = "token 21HEK7SCCDNT12APRN00YJIY4AIYZM3DLXPNT8NP1275FA9MRWJZDM2ICWT8MYJX"
+# TODO: this is a test propose token
 
 
 class MyServer(BaseHTTPRequestHandler):
@@ -27,8 +28,10 @@ class MyServer(BaseHTTPRequestHandler):
                 to_print = json.dumps(info, sort_keys=True, indent=2)
                 print(to_print)
                 print("======================================")
-                pagure_payload = {'title': info['title'], 'issue_content': info['content'],
-                               'status': "Open"}
+                pagure_title = '[' + str(info['id']) + ']: ' + info['title'] + ' by ' + info['creator']
+                # TODO: now containing all the metadata in the title, should use a more elegant solution
+                pagure_payload = {'title': pagure_title, 'issue_content': info['content'],
+                                  'status': "Open"}
                 pagure_URL = "https://pagure.io/api/0/doc-test/new_issue"
                 pagure_head = {"Authorization": pagureToken}
                 r = requests.post(pagure_URL, data=pagure_payload, headers=pagure_head)
