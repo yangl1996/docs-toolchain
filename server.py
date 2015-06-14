a5411742b859ba17f83a7fa53dc097d869900913
Lei Yang <yltt1234512@gmail.com>
2015-06-14 13:55:34 +0800

    centralize all configs

--- server.py
+++ server.py
@@ -8,12 +8,14 @@ listenPort = 7654
 secretKey = ""  # TODO:implement the secure feature of GitHub web hook (verify SHA1 signature)
 pagureToken = "token 21HEK7SCCDNT12APRN00YJIY4AIYZM3DLXPNT8NP1275FA9MRWJZDM2ICWT8MYJX"
 # TODO: this is a test propose token
+pagureRepo = "doc-test"
 
 
 class MyServer(BaseHTTPRequestHandler):
     def do_POST(self):
         # TODO: send a response to github after receiving the request
         global pagureToken
+        global pagureRepo
 
         content_len = int(self.headers['content-length'])
         post_body = self.rfile.read(content_len).decode()
@@ -33,7 +35,7 @@ class MyServer(BaseHTTPRequestHandler):
                 # TODO: now containing all the metadata in the title, should use a more elegant solution
                 pagure_payload = {'title': pagure_title, 'issue_content': info['content'],
                                   'status': "Open"}
-                pagure_URL = "https://pagure.io/api/0/doc-test/new_issue"
+                pagure_URL = "https://pagure.io/api/0/" + pagureRepo + "/new_issue"
                 pagure_head = {"Authorization": pagureToken}
                 r = requests.post(pagure_URL, data=pagure_payload, headers=pagure_head)
                 print(r.text)
