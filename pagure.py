5e79790397d4fe2497ab650e47b6e88a7a092fc2
Lei Yang <yltt1234512@gmail.com>
2015-08-05 17:09:42 +0800

    use name space 'config'

--- pagure.py
+++ pagure.py
@@ -13,17 +13,8 @@ except "No such file or directory":
     exit()
 
 # import configurations from config.py
-listenAddr = config.listenAddr
-listenPort = config.pagurePort
-databasePath = config.databasePath
-pagureRepo = config.pagureRepo
-pagureToken = config.pagureToken
-pagureHeader = {"Authorization": "token " + pagureToken}
-pagureSecretKey = config.pagureSecretKey
-githubToken = config.githubToken
-githubHeader = {"Authorization": "token " + githubToken}
-githubUsername = config.githubUsername
-githubRepo = config.githubRepo
+pagureHeader = {"Authorization": "token " + config.pagureToken}
+githubHeader = {"Authorization": "token " + config.githubToken}
 
 # TODO: how to handle merge conflict
 
@@ -35,19 +26,23 @@ def handle_edition(post_body):
     data = json.loads(post_body)  # parse web hook payload
     if data['msg']['issue']['status'] == 'Fixed':
         logging.info("An issue is marked as fixed on Pagure.")
-        conn = sqlite3.connect(databasePath)
+        conn = sqlite3.connect(config.databasePath)
         c = conn.cursor()
         c.execute('SELECT * FROM Requests WHERE PagureID=?', (data['msg']['issue']['id'],))
         entry = c.fetchone()
         pr_id = int(entry[2])
         conn.close()
-        r = requests.get("https://api.github.com/repos/{}/{}/pulls/{}".format(githubUsername, githubRepo, pr_id),
+        r = requests.get("https://api.github.com/repos/{}/{}/pulls/{}".format(config.githubUsername,
+                                                                              config.githubRepo,
+                                                                              pr_id),
                          headers=githubHeader)  # get PR info from github
         return_data = json.loads(r.text)  # parse PR info
         pr_sha = return_data['head']['sha']  # get PR sha from PR info
         # call github API to merge the PR
         merge_payload = json.dumps({"commit_message": "Merge pull request" + str(pr_id), "sha": pr_sha})
-        requests.put('https://api.github.com/repos/{}/{}/pulls/{}/merge'.format(githubUsername, githubRepo, pr_id),
+        requests.put('https://api.github.com/repos/{}/{}/pulls/{}/merge'.format(config.githubUsername,
+                                                                                config.githubRepo,
+                                                                                pr_id),
                      headers=githubHeader, data=merge_payload)
         # TODO: add a commment on github to remind to delete the branch
 
@@ -59,7 +54,7 @@ def handle_added(post_body):
     # TODO: handle issues added on pagure (sync to GitHub issue?)
     if added_title.startswith("#"):
         logging.info("An mirror issue is added on Pagure.")
-        conn = sqlite3.connect(databasePath)
+        conn = sqlite3.connect(config.databasePath)
         c = conn.cursor()
         c.execute('UPDATE Requests SET PagureID=? WHERE PagureTitle=?', (added_id,
                                                                          added_title,))
@@ -69,11 +64,11 @@ def handle_added(post_body):
         conn.commit()
         conn.close()
         # call github API to post a comment containing pagure issue link to github PR
-        pr_comment_link = "https://api.github.com/repos/{}/{}/issues/{}/comments".format(githubUsername,
-                                                                                         githubRepo,
+        pr_comment_link = "https://api.github.com/repos/{}/{}/issues/{}/comments".format(config.githubUsername,
+                                                                                         config.githubRepo,
                                                                                          pr_id)
         pr_comment_body = "[Issue #{}](https://pagure.io/{}/issue/{}) created on Pagure.".format(added_id,
-                                                                                                 pagureRepo,
+                                                                                                 config.pagureRepo,
                                                                                                  added_id)
         github_payload = {"body": pr_comment_body}
         requests.post(pr_comment_link, data=json.dumps(github_payload), headers=githubHeader)
@@ -93,7 +88,7 @@ def handle_comment(post_body):
     comment_body = """*Commented by {} ({})*
 
     {}""".format(info['fullname'], info['username'], info['comment'])  # generate comment body to be posted to github
-    conn = sqlite3.connect(databasePath)
+    conn = sqlite3.connect(config.databasePath)
     c = conn.cursor()
     c.execute('SELECT * FROM Requests WHERE PagureID=?', (data['msg']['issue']['id'],))
     entry = c.fetchone()
@@ -101,7 +96,9 @@ def handle_comment(post_body):
     conn.close()
     # call github API to post the comment
     comment_payload = json.dumps({"body": comment_body})
-    requests.post("https://api.github.com/repos/{}/{}/issues/{}/comments".format(githubUsername, githubRepo, pr_id),
+    requests.post("https://api.github.com/repos/{}/{}/issues/{}/comments".format(config.githubUsername,
+                                                                                 config.githubRepo,
+                                                                                 pr_id),
                   headers=githubHeader, data=comment_payload)
 
 
@@ -138,10 +135,10 @@ class MyServer(BaseHTTPRequestHandler):
             th.start()
 
 
-myServer = HTTPServer((listenAddr, listenPort), MyServer)
+myServer = HTTPServer((config.listenAddr, config.pagurePort), MyServer)
 print("Test Server")
 logging.basicConfig(filename='pagure.log', level=logging.INFO)
-logging.info('Server starts ay %s:%s.', listenAddr, listenPort)
+logging.info('Server starts ay %s:%s.', config.listenAddr, config.pagurePort)
 
 try:
     myServer.serve_forever()
