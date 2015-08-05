b8a9b8919a669930ee6d66034924076566de6fc9
Lei Yang <yltt1234512@gmail.com>
2015-08-05 23:20:55 +0800

    specify branch when pulling

--- git_interface.py
+++ git_interface.py
@@ -15,7 +15,7 @@ class Repository:
             remote = self.__remote_1
         elif remote_index == 2:
             remote = self.__remote_2
-        command = """cd {} \n git checkout master \n git pull {} \n""".format(self.__path, remote)
+        command = """cd {} \n git checkout master \n git pull {} master \n""".format(self.__path, remote)
         os.system(command)
 
     def push(self, remote_index):
