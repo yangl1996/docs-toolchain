8a9bfc216577eda47c52ff2105c38c25e31ddb19
Lei Yang <yltt1234512@gmail.com>
2015-08-07 03:12:47 +0800

    remove debug lines

--- github.py
+++ github.py
@@ -45,9 +45,7 @@ def ci_build(pull_request_id, patch_url):
     f.write(patch_data.read().decode('utf-8'))
     f.close()
 
-    print("before all")
     gitRepository.apply("localdata/{}/{}".format(pull_request_id, patch_file))
-    print("before two")
 
     filelist = []
     for changed_file in data:
@@ -70,7 +68,6 @@ def ci_build(pull_request_id, patch_url):
         finally:
             filelist.append(this_file)
     gitRepository.apply("localdata/{}/{}".format(pull_request_id, patch_file), True)
-    print("after all")
     filelist_name = "filelist-pr-{}.json".format(pull_request_id)
     with open(patch_path + '/' + filelist_name, 'w') as f:
         json.dump(filelist, f)
